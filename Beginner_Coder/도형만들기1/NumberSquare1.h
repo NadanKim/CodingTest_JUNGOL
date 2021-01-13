f813b394660aa5d77bed5f55ffa72dd675b47217
김나단 <paintime@naver.com>
2021-01-13 12:25:48 +0900

    풀이완료 Beginner_Coder/도형만들기1/NumberSquare1, NumberSquare2, NumberSquare3, NumberSquare4

---
+++ Beginner_Coder/도형만들기1/NumberSquare1.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <iostream>
+
+#include "../../Base.h"
+
+class NumberSquare1 : public Base
+{
+protected:
+	void Code() override;
+};
--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
 ﻿// 문제 클래스 바꾸는 법
 // 1. 원하는 문제 클래스의 헤더를 불러온다.
 // 2. codeBase에 문제 클래스를 생성한다.
-#include "Beginner_Coder/도형만들기1/TimesTable2.h"
+#include "Beginner_Coder/도형만들기1/NumberSquare4.h"
 
 int main()
 {
-    Base* codeBase = new TimesTable2();
+    Base* codeBase = new NumberSquare4();
     codeBase->Code();
 
     delete codeBase;
