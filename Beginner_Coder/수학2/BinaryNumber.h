10419f9fb76cb93758e52dcc0188b3688454e5cb
김나단 <paintime@naver.com>
2021-04-10 10:50:48 +0900

    풀이완료 Beginner_Coder/수학2/BinaryNumber

---
+++ Beginner_Coder/수학2/BinaryNumber.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <cmath>
+
+#include "../../Base.h"
+
+using std::string;
+
+class BinaryNumber : public Base
+{
+protected:
+	void Code() override;
+};
