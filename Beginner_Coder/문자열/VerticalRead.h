7a38b751f37ef44b1b420fd18d6e5995a7b16089
김나단 <paintime@naver.com>
2021-04-24 17:53:42 +0900

    풀이완료 Beginner_Coder/문자열/VerticalRead

---
+++ Beginner_Coder/문자열/VerticalRead.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <iostream>
+#include <string>
+
+#include "../../Base.h"
+
+using std::string;
+
+class VerticalRead : public Base
+{
+protected:
+	void Code() override;
+};
--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
 ﻿// 문제 클래스 바꾸는 법
 // 1. 원하는 문제 클래스의 헤더를 불러온다.
 // 2. codeBase에 문제 클래스를 생성한다.
-#include "Beginner_Coder/문자열/FindString.h"
+#include "Beginner_Coder/문자열/VerticalRead.h"
 
 int main()
 {
-    Base* codeBase = new FindString();
+    Base* codeBase = new VerticalRead();
     codeBase->Code();
 
     delete codeBase;
