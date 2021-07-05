02bc62b663f2afa6a1c16184a008b0cf3efefe68
김나단 <paintime@naver.com>
2021-07-05 11:43:53 +0900

    풀이완료 Beginner_Coder/자료처리/InsertionSort

--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
 ﻿// 문제 클래스 바꾸는 법
 // 1. 원하는 문제 클래스의 헤더를 불러온다.
 // 2. codeBase에 문제 클래스를 생성한다.
-#include "Beginner_Coder/자료처리/SelectionSort.h"
+#include "Beginner_Coder/자료처리/InsertionSort.h"
 
 int main()
 {
-    Base* codeBase = new SelectionSort();
+    Base* codeBase = new InsertionSort();
     codeBase->Code();
 
     delete codeBase;
