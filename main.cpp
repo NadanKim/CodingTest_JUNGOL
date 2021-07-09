24b513a13d0e151f3f4d40f8296ee74ca383aa2b
김나단 <paintime@naver.com>
2021-07-09 12:10:00 +0900

    풀이완료 Beginner_Coder/자료처리/PostfixNotation1

--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
 ﻿// 문제 클래스 바꾸는 법
 // 1. 원하는 문제 클래스의 헤더를 불러온다.
 // 2. codeBase에 문제 클래스를 생성한다.
-#include "Beginner_Coder/자료처리/CountInsertionSort.h"
+#include "Beginner_Coder/자료처리/PostfixNotation1.h"
 
 int main()
 {
-    Base* codeBase = new CountInsertionSort();
+    Base* codeBase = new PostfixNotation1();
     codeBase->Code();
 
     delete codeBase;
