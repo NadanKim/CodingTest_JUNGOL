7f199fae337f00fa4b5b6d94a4816f193835d2ee
김나단 <paintime@naver.com>
2021-09-16 01:52:01 +0900

    임시저장 Intermediate_Coder/백트래킹-DFS/NQueen

--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
 ﻿// 문제 클래스 바꾸는 법
 // 1. 원하는 문제 클래스의 헤더를 불러온다.
 // 2. codeBase에 문제 클래스를 생성한다.
-#include "Intermediate_Coder/자료구조/ColoredPaper3.h"
+#include "Intermediate_Coder/백트래킹-DFS/NQueen.h"
 
 int main()
 {
-    Base* codeBase = new ColoredPaper3();
+    Base* codeBase = new NQueen();
     codeBase->Code();
 
     delete codeBase;
