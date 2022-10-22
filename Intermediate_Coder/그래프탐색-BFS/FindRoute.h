e8da129dcad8d8875dad9d1e61b1623abf198c4a
nadankim <paintime@naver.com>
2022-10-22 18:02:22 +0900

    새 파일 추가

---
+++ Intermediate_Coder/그래프탐색-BFS/FindRoute.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <iostream>
+#include <queue>
+#include <string>
+
+#include "../../Base.h"
+
+using std::queue;
+using std::string;
+
+class FindRoute : public Base
+{
+protected:
+	void Code() override;
+};
