c1b4e512d08b53710022d7206769d0a22d034a91
niamdank <niamdank@gmail.com>
2023-01-20 11:03:26 +0900

    JUNGOL 문제 풀이 클래스 추가
    숫자와 함께 하는 여행

---
+++ Intermediate_Coder/그래프탐색-BFS/TravelWithPrimeNumber.h
@@ -0,0 +1,51 @@
+#pragma once
+#include <iostream>
+#include <queue>
+#include <vector>
+#include <algorithm>
+#include <string>
+
+#include "../../Base.h"
+
+using std::queue;
+using std::vector;
+using std::string;
+
+class TravelWithPrimeNumber : public Base
+{
+protected:
+	struct Route
+	{
+		int index = 0;
+		vector<int> route;
+		
+		Route() {}
+		Route(int index) : index(index) { route.push_back(index); }
+		Route(int index, const Route& other)
+			: index(index)
+		{
+			for (int otherIndex : other.route)
+			{
+				route.push_back(otherIndex);
+			}
+			route.push_back(index);
+		}
+	};
+
+public:
+	virtual void Code() override;
+
+private:
+	bool FindHamingRoute();
+	bool IsHamingDistance(int index1, int index2);
+
+private:
+	int a, b;
+
+	vector<string> allData;
+	queue<Route> q;
+
+	Route result;
+
+	vector<bool> checkList;
+};
--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
 ﻿// 문제 클래스 바꾸는 법
 // 1. 원하는 문제 클래스의 헤더를 불러온다.
 // 2. codeBase에 문제 클래스를 생성한다.
-#include "Intermediate_Coder/그래프탐색-BFS/FindRoute.h"
+#include "Intermediate_Coder/그래프탐색-BFS/TravelWithPrimeNumber.h"
 
 int main()
 {
-    Base* codeBase = new FindRoute();
+    Base* codeBase = new TravelWithPrimeNumber();
     codeBase->Code();
 
     delete codeBase;
